439bd1ae5b59b35095b904e38aef2f242d572cdb
cozyGarage <personal.trungnguyen@gmail.com>
2023-05-22 15:43:47 +0300

    Add project files.

---
+++ Test.Unit.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test.Unit.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("VAMK")]
+[assembly: AssemblyProduct("Test.Unit.Test")]
+[assembly: AssemblyCopyright("Copyright © VAMK 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("03fac2f5-57cc-4424-855e-dcbe87e2699a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
